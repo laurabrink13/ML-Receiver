faa7443feb83c1981703ae7d63aa6b3a43cc2d64
Dat Nguyen <tdat.nguyen93@gmail.com>
2018-08-24 23:56:07 -0700

    Updated Metrics and Helper methods

--- radioml/metrics.py
+++ radioml/metrics.py
@@ -1,15 +1,31 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import numpy as np
 import commpy as cp
 
+
 def get_ber_bler(estimated_bits, original_bits):
-    """Compute Bit Error Rate and Block Error Rate."""
-    n_sequences = len(original_bits)
+    """Compute Bit Error Rate and Block Error Rate.
+
+    Arguments:
+    ----------
+        estimated_bits: int - size [batch, data_len]
+        original_bits:  int - size [batch, data_len]
+
+    Returns:
+        ber: float - Bit Error Rate
+        bler: float - Block Error Rate
+    """
+
     hamming_distances = []
-    for i in range(n_sequences):       
+    for i in range(len(original_bits)):
         dist = cp.utilities.hamming_dist(
             original_bits[i].astype(int),
             estimated_bits[i].astype(int))
         hamming_distances.append(dist)
+        
     ber = np.sum(hamming_distances) / np.product(np.shape(original_bits))
     bler = np.count_nonzero(hamming_distances) / len(original_bits)
     return ber, bler
--- radioml/utils.py
+++ radioml/utils.py
@@ -1,13 +1,61 @@
 import os
+import numpy as np
 import tensorflow as tf
-from tensorflow.python.ops import array_ops
-from tensorflow.python.keras import backend as K
-from tensorflow.python.summary import summary as tf_summary
+from commpy.modulation import QAMModem
+from commpy.channelcoding import Trellis
+
+
+def build_trellis_structure(memory=np.array([2]),
+                            g_matrix=np.array([[0o7, 0o5]])):
+    """Our current assumption about how input is encoded. All signals are encoded
+    using Convolution Code at rate 1/2.
+
+    Arguments:
+    ----------
+        memory : 1D ndarray of ints
+            Number of memory elements per input of the convolutional encoder.
+
+        g_matrix : 2D ndarray of ints (octal representation)
+            Generator matrix G(D) of the convolutional encoder. Each element of
+            G(D) represents a polynomial.
+
+    Returns:
+    --------
+        trellis - a Trellis structure
+    """
+    return Trellis(memory=memory, g_matrix=g_matrix)
+
+
+def build_modulator(modulation_scheme):
+    """Construct a modulator.
+
+    Arguments:
+    ----------
+        modulation_scheme: a string - name of a modulation scheme.
+
+    Returns:
+    --------
+        QAMModem - represents  modulator @ particular modulation scheme.
+    """
+    # @TODO: add more modulation schemes
+
+    if str.lower(modulation_scheme) == 'qpsk':
+        return QAMModem(m=4)
+    elif str.lower(modulation_scheme) == 'qam16':
+        return QAMModem(m=16)
+    elif str.lower(modulation_scheme) == 'qam64':
+        return QAMModem(m=64)
+    elif str.lower(modulation_scheme) == 'qam128':
+        return QAMModem(m=128)
+    else:
+        raise ValueError('Modulation scheme {} is not supported'.format(modulation_scheme))
 
 
 class TrainValTensorBoard(tf.keras.callbacks.TensorBoard):
-    """Write summaries with training and evaluation in
-    on plot. Display images on visualization"""
+    """Write summaries with training and evaluation in on plot.
+
+
+    """
     def __init__(self, log_dir='./logs', **kwargs):
         # Make the original `TensorBoard` log to a subdirectory 'training'
         training_log_dir = os.path.join(log_dir, 'training')
@@ -45,7 +93,7 @@ class TrainValTensorBoard(tf.keras.callbacks.TensorBoard):
 
 
 def visualize_signals(ax, x, y, groundtruths=None, title=None, min_val=-2, max_val=2):
-    ax.scatter(x, y, c=groundtruths)
+    ax.scatter(x, y, c=groundtruths, s=300)
     ax.set_xlabel('I-component')
     ax.set_ylabel('Q-component')
     ax.set_title(title)
